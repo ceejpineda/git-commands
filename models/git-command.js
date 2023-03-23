72d032cd2d8098a847e73f6bf5827d6ac3860404
unknown <pinedacj1020@gmail.com>
2023-03-23 22:23:07 +0800

    test #5

--- models/git-command.js
+++ models/git-command.js
@@ -16,7 +16,7 @@ class GitCommand {
         */
         this.init();
         for(let file of Object.keys(this.working_directory.files)){
-            this.add(file); //Test workflow #4
+            this.add(file); //Test workflow #45
         };
         if(this.staging.length == 0){
             return 'You have 0 change/s.\n';
